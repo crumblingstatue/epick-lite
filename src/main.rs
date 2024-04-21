3e32aae85dddd599b6bcf68fb4e5d1a992d1fd63
crumblingstatue <radiantstatue@gmail.com>
2024-04-22 00:07:12 +0200

    Rename fork to epick-lite

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,41 @@
-const APP_CANVAS_ID: &str = "epick - Color Picker";
+mod app;
+mod color;
+mod color_picker;
+mod context;
+mod display_picker;
+mod error;
+mod keybinding;
+mod math;
+mod render;
+mod screen_size;
+mod settings;
+mod ui;
+mod zoom_picker;
+
+use anyhow::{Context, Error};
+
+fn save_to_clipboard(text: String) -> Result<(), Error> {
+    let mut clipboard = arboard::Clipboard::new()?;
+    clipboard
+        .set_text(text)
+        .context("failed to save to clipboard")
+}
+
+fn get_timestamp() -> u64 {
+    use std::time::{SystemTime, UNIX_EPOCH};
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .map(|d| d.as_secs())
+        .unwrap_or(1)
+}
+
+fn elapsed(timestamp: u64) -> u64 {
+    get_timestamp() - timestamp
+}
+
+const APP_CANVAS_ID: &str = "epick-lite - Color Picker";
 
 fn main() {
-    use eframe::NativeOptions;
-
-    /*const APP_ICON_DATA: &[u8] = include_bytes!("../assets/icon.png");
-    const APP_ICON_WIDTH: u32 = 48;
-    const APP_ICON_HEIGHT: u32 = APP_ICON_WIDTH;*/
-    let opts = NativeOptions::default();
-
-    //pretty_env_logger::init();
-    //let subscriber = tracing_subscriber::fmt()
-    //.with_file(true)
-    //.with_target(true)
-    //.with_line_number(true)
-    //.pretty()
-    //.with_max_level(tracing::Level::TRACE)
-    //.finish();
-    //tracing::subscriber::set_global_default(subscriber).unwrap();
-
-    // TODO: Fix for egui 0.26
-    /*let mut img = ImageReader::new(Cursor::new(APP_ICON_DATA));
-    img.set_format(ImageFormat::Png);
-    match img
-        .decode()
-        .map(|img| img.as_rgba8().map(|img| img.to_vec()))
-    {
-        Ok(Some(rgba)) => {
-            opts.icon_data = Some(IconData {
-                rgba,
-                width: APP_ICON_WIDTH,
-                height: APP_ICON_HEIGHT,
-            })
-        }
-        Err(e) => {
-            eprintln!("failed to load app icon data - {}", e);
-        }
-        _ => {}
-    }*/
-
-    eframe::run_native(APP_CANVAS_ID, opts, Box::new(|ctx| epick::Epick::init(ctx))).unwrap();
+    let opts = eframe::NativeOptions::default();
+    eframe::run_native(APP_CANVAS_ID, opts, Box::new(|ctx| app::App::init(ctx))).unwrap();
 }
